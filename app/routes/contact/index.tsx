5c1115b3de2e268180c23005714f4a7a9b2c2985
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2025-07-30 13:35:59 +0100

    Create a contact form and make it submit an action

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,11 +1,97 @@
+import type { Route } from "./+types";
+import { Form } from "react-router";
 
-function ContactPage() {
+export async function action({ request }: Route.ActionArgs) {
+  // Submit form data to the server
+
+  // Get the formData from the Form component
+  const formData = await request.formData();
+
+  // Get the values of the inputs using formData
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const subject = formData.get("subject");
+  const message = formData.get("message");
+
+  // Create a data object
+  const data = {
+    name,
+    email,
+    subject,
+    message
+  };
+
+  return { message: "Form Submitted Successfully", data };
+}
+
+// Pass the actionData as a prop from the action function to the ContactPage
+function ContactPage({ actionData }: Route.ComponentProps) {
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
       <h2 className='text-3xl font-bold text-white mb-8 text-center'>
         Contact Me
       </h2>
-    </>
+
+      { actionData?.message && (
+        <p
+          className="mb-6 py-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md"
+        >
+          {actionData.message}
+        </p>
+      )}
+      <Form method="post" className="space-y-6">
+        <div>
+          <label htmlFor="name" className="block text-sm font-medium text-gray-300">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium text-gray-300">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label htmlFor="subject" className="block text-sm font-medium text-gray-300">
+            Subject
+          </label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label htmlFor="message" className="block text-sm font-medium text-gray-300">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="cursour-pointer w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700"
+        >
+          Send Message
+        </button>
+      </Form>
+    </div>
   )
 }
 
